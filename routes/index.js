867b740421cd8ba54a0d7bc9ac7d87f5678f7de0
ant-mejia <ps@antmejia.com>
2017-01-26 11:37:39 -0500

    Added forEach to Users table

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../server/models/index');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/users');
+  models.User.findAll({}).then(function(users) {
+    res.render('users/index', {
+      title: 'fazbook',
+      users: users
+    });
+  });
 });
 
 module.exports = router;
